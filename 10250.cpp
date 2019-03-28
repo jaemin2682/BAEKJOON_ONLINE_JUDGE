f37ffcfe65a1a21c980fdfe6519293af0f86ac2e
jaemin2682 <46991162+jaemin2682@users.noreply.github.com>
2019-03-28 17:30:02 +0900

    Create 10250.cpp
    10250번
    title : ACM호텔

---
+++ 10250.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t, h, w, n;
+	int realh, realw;
+	scanf("%d", &t);
+
+	for (int i = 0; i < t; i++) {
+		scanf("%d %d %d", &h, &w, &n);
+		realw = 1;
+		for (int j = 1;; j++) {
+			if (n > h) {
+				n = n - h;
+				realw++;
+			}
+			else {
+				realh = n;
+				break;
+			}
+		}
+		printf("%d\n", realh * 100 + realw);
+	}
+}
