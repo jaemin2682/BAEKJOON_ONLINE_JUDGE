86143f2a1bdbd5796ad5b54672cb098aec326d6a
jaemin2682 <46991162+jaemin2682@users.noreply.github.com>
2019-10-19 19:15:53 +0900

    아스키 코드

---
+++ 11654.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main() {
+	char a;
+	cin >> a;
+
+	cout << (int)a;
+
+	return 0;
+}
