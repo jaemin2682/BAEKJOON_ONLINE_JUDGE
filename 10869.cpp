3bd57b8344a833f0746231ee198252c700de6d2b
jaemin2682 <46991162+jaemin2682@users.noreply.github.com>
2019-11-04 14:04:37 +0900

    사칙연산

---
+++ 10869.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main() {
+	int a, b;
+	cin >> a >> b;
+	cout << a + b << endl;
+	cout << a - b << endl;
+	cout << a * b << endl;
+	cout << a / b << endl;
+	cout << a % b << endl;
+	return 0;
+}
