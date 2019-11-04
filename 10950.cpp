a7f9d33d5fdc335f1c6144760164702e5023cd4d
jaemin2682 <46991162+jaemin2682@users.noreply.github.com>
2019-11-04 14:58:37 +0900

    A+B - 3

---
+++ 10950.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	for (int i = 0; i < t; i++) {
+		int a, b;
+		cin >> a >> b;
+		cout << a + b << endl;
+	}
+
+	return 0;
+}
