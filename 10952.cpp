18aa61666050eff994d5221533d0a7c51a43457e
jaemin2682 <46991162+jaemin2682@users.noreply.github.com>
2019-11-04 15:50:42 +0900

    A+B - 5

---
+++ 10952.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main() {
+	while (1) {
+		int a, b;
+		cin >> a >> b;
+		if (a == 0 && b == 0) break;
+		cout << a + b << endl;
+	} 
+
+	return 0;
+}
