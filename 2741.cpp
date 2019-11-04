97249d4e5d4d357a7e77bf08f20a6aed173e4661
jaemin2682 <46991162+jaemin2682@users.noreply.github.com>
2019-11-04 15:16:32 +0900

    N 찍기

---
+++ 2741.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main() {
+	int n;
+	scanf("%d", &n);
+	for (int i = 1; i <= n; i++) {
+		printf("%d\n", i);
+	}
+
+
+	return 0;
+}
