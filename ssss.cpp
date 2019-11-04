1b203f8bdd21e5f47b47b17f4462040b065f3835
jaemin2682 <46991162+jaemin2682@users.noreply.github.com>
2019-11-04 15:33:19 +0900

    A+B - 7

---
+++ ssss.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main() {
+	int a, b, t;
+	cin >> t;
+
+	for (int i = 0; i < t; i++) {
+		cin >> a >> b;
+		cout << "Case #" << i + 1 << ": " << a + b << endl;
+	}
+
+	return 0;
+}
