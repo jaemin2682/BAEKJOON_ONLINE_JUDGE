1fbc1af114f05b4d5d00214f53ed3c29f2003371
jaemin2682 <46991162+jaemin2682@users.noreply.github.com>
2019-11-04 13:56:47 +0900

    고양이

---
+++ 10171.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main() {
+	cout << "\\    /\\" << endl;
+	cout << " )  ( ')" << endl;
+	cout << "(  /  )" << endl;
+	cout << " \\(__)|" << endl;
+	return 0;
+}
