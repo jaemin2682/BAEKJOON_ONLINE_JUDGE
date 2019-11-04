3d1825e801811d5c6145b97dc640bb7ccc5ddefc
jaemin2682 <46991162+jaemin2682@users.noreply.github.com>
2019-11-04 15:13:30 +0900

    빠른 A+B

---
+++ 15552.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main() {
+	int t;
+	scanf("%d", &t);
+	for (int i = 0; i < t; i++) {
+		int a, b;
+		scanf("%d %d", &a, &b);
+		printf("%d\n", a + b);
+	}
+
+
+	return 0;
+}
