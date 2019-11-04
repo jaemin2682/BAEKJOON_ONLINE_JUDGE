c201f96178a3c2de951437161a21c28a90c430f3
jaemin2682 <46991162+jaemin2682@users.noreply.github.com>
2019-11-04 14:02:12 +0900

    A+B

---
+++ 1000.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main() {
+	int a, b;
+	cin >> a >> b;
+	cout << a + b;
+	return 0;
+}
