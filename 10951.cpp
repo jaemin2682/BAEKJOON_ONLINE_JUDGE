92700aba6d54c334045baaf7e1f4d02c468bedd9
jaemin2682 <46991162+jaemin2682@users.noreply.github.com>
2019-11-04 15:57:46 +0900

    A+B - 4

---
+++ 10951.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main() {
+	int a, b;
+	while (cin >> a >> b) {
+		cout << a + b << endl;
+	} 
+
+	return 0;
+}
