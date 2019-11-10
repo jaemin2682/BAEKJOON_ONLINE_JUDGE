33911edf55b331f4ff57482a49c9b79b26486abb
jaemin2682 <46991162+jaemin2682@users.noreply.github.com>
2019-11-10 15:06:49 +0900

    단어 길이 재기

---
+++ 2743.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <memory.h>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+	string str;
+	cin >> str;
+	cout << str.size();
+
+	return 0;
+}
+
