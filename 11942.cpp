376d2efb5e67e5449486b92cc7bc7864e63d0a70
jaemin2682 <46991162+jaemin2682@users.noreply.github.com>
2019-11-11 03:43:36 +0900

    고려대는 사랑입니다

---
+++ 11942.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <memory.h>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+	cout << "고려대학교";
+
+	return 0;
+}
+
