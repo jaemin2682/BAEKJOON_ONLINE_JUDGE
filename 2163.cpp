26523826c37ebad97919502e2c9c0abd76cbed62
jaemin2682 <46991162+jaemin2682@users.noreply.github.com>
2019-11-11 03:29:00 +0900

    초콜릿 자르기

---
+++ 2163.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <memory.h>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+	int n, m;
+	cin >> n >>  m;
+	cout << n * m - 1;
+	return 0;
+}
+
