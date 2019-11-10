7e20f728a287841b1274c130f9d098a9362ce0e9
jaemin2682 <46991162+jaemin2682@users.noreply.github.com>
2019-11-11 03:22:13 +0900

    별 찍기 - 7

---
+++ 2444.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <memory.h>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	int cntblank = n - 1;
+	int cntstar = 1;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < cntblank; j++) cout << ' ';
+		for (int j = 0; j < cntstar; j++) cout << '*';
+		cntblank--;
+		cntstar += 2;
+		cout << endl;
+	}
+	cntblank = 1;
+	cntstar = 2*n - 3;
+	for (int i = 0; i < n - 1; i++) {
+		for (int j = 0; j < cntblank; j++) cout << ' ';
+		for (int j = 0; j < cntstar; j++) cout << '*';
+		cntblank++;
+		cntstar -= 2;
+		cout << endl;
+	}
+	
+	return 0;
+}
