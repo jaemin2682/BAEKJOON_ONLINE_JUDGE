6a60c1fdef27b36908405669d67889b15d3ea9af
jaemin2682 <46991162+jaemin2682@users.noreply.github.com>
2019-11-10 16:03:36 +0900

    R2

---
+++ 3046.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <memory.h>
+#include <math.h>
+
+
+using namespace std;
+
+int main() {
+	int r1, r2, s;
+	cin >> r1 >> s;
+	
+	cout << s * 2 - r1;
+	return 0;
+}
+
+
