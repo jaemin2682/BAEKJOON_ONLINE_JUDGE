0743647891e5e07d12f2f3c6b823438d6dc231b2
jaemin2682 <46991162+jaemin2682@users.noreply.github.com>
2019-11-11 22:43:49 +0900

    웰컴

---
+++ 5337.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <memory.h>
+
+using namespace std;
+
+int main() {
+	cout << ".  .   ." << endl;
+	cout << "|  | _ | _. _ ._ _  _" << endl;
+	cout << "|/\\|(/.|(_.(_)[ | )(/." << endl;
+
+	return 0;
+}
