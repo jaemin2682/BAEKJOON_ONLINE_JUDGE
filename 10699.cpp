b582db4bd96ca01687919f6e5219cd7996707aad
jaemin2682 <46991162+jaemin2682@users.noreply.github.com>
2019-11-11 17:29:11 +0900

    오늘 날짜

---
+++ 10699.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <memory.h>
+
+using namespace std;
+
+int main() {
+	cout << "2019-11-11";
+
+	return 0;
+}
