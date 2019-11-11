b130d457cd5436013dbe30d8f876e0ca69eb0be9
jaemin2682 <46991162+jaemin2682@users.noreply.github.com>
2019-11-11 20:00:23 +0900

    콜센터

---
+++ 5339.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <memory.h>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+	cout << "     /~\\" << endl;
+	cout << "    ( oo|" << endl;
+	cout << "    _\\=/_" << endl;
+	cout << "   /  _  \\" << endl;
+	cout << "  //|/.\\|\\\\ " << endl;
+	cout << " ||  \\ /  ||" << endl;
+	cout << "============" << endl;
+	cout << "|          |" << endl;
+	cout << "|          |" << endl;
+	cout << "|          |" << endl;
+
+
+	return 0;
+}
