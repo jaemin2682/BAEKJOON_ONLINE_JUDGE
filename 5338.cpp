c4cdbd8d375e3f53d9752a950991dfaecb12a173
jaemin2682 <46991162+jaemin2682@users.noreply.github.com>
2019-11-11 15:38:43 +0900

    마이크로소프트 로고

---
+++ 5338.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <memory.h>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+	cout << "       _.-;;-._" << endl;
+	cout << "'-..-'|   ||   |" << endl;
+	cout << "'-..-'|_.-;;-._|" << endl;
+	cout << "'-..-'|   ||   |" << endl;
+	cout << "'-..-'|_.-''-._|" << endl;
+
+	return 0;
+}
+
