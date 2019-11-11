9f653f590a02cfec5f4a96aaa6701d52cc0bdc5a
jaemin2682 <46991162+jaemin2682@users.noreply.github.com>
2019-11-12 00:15:53 +0900

    저작권

---
+++ 2914.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <memory.h>
+
+using namespace std;
+
+int main() {
+	int a, i;
+	cin >> a >> i;
+
+	cout << a * (i - 1) + 1;
+	return 0;
+}
