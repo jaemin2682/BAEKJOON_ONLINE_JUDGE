130b4a7d3fbc5eee0fbcd2a2c7c85f12056cc616
jaemin2682 <46991162+jaemin2682@users.noreply.github.com>
2019-11-12 22:36:21 +0900

    더하기

---
+++ 14918.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <memory.h>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+	int a, b;
+	cin >> a >> b;
+	cout << a + b;
+	return 0;
+}
