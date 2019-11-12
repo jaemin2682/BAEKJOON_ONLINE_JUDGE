718ac3da9810770b613ca0fe0c21afed0fe0da14
jaemin2682 <46991162+jaemin2682@users.noreply.github.com>
2019-11-13 00:23:55 +0900

    팬들에게 둘러싸인 홍준

---
+++ 14581.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <memory.h>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+	string str;
+	cin >> str;
+	cout << ":fan::fan::fan:" << endl;
+	cout << ":fan::" << str << "::fan:" << endl;
+	cout << ":fan::fan::fan:";
+
+	return 0;
+}
