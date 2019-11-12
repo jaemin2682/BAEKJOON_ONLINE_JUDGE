ea3b948d861b2991701800c10899aea9a2cafd47
jaemin2682 <46991162+jaemin2682@users.noreply.github.com>
2019-11-13 01:04:42 +0900

    나는 누구인가

---
+++ 15733.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <memory.h>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+	cout << "I'm Sexy";
+
+	return 0;
+}
