b87ee3281c062a5cb25f55999a4f5b49d9d6bd64
jaemin2682 <46991162+jaemin2682@users.noreply.github.com>
2019-11-13 01:16:41 +0900

    이상한 기호

---
+++ 15964.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <memory.h>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+	long long a, b;
+	cin >> a >> b;
+	cout << (a + b)*(a - b);
+	return 0;
+}
