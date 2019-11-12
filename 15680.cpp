5f49f9ad2ac5cfbfa5136942306924eab7dd9918
jaemin2682 <46991162+jaemin2682@users.noreply.github.com>
2019-11-12 22:12:07 +0900

    연세대학교

---
+++ 15680.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <memory.h>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	if (n == 0) cout << "YONSEI";
+	else cout << "Leading the Way to the Future";
+
+	return 0;
+}
