ee4d46e294407ee3a7814e28a7a2e7ab4a6ca8c8
jaemin2682 <46991162+jaemin2682@users.noreply.github.com>
2019-11-14 00:25:52 +0900

    꼬마 정민

---
+++ 11382.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <memory.h>
+#include <math.h>
+#include <set>
+
+using namespace std;
+
+int main() {
+	long long a, b, c;
+	cin >> a >> b >> c;
+	cout << a + b + c;
+	return 0;
+}
