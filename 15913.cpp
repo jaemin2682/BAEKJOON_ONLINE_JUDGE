75db7e6d1ca2810d9fb08bd249aa6ff37e64f5e5
jaemin2682 <46991162+jaemin2682@users.noreply.github.com>
2019-11-15 00:47:57 +0900

    가위 바위 보 999

---
+++ 15913.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <memory.h>
+#include <math.h>
+#include <queue>
+
+using namespace std;
+
+int main() {
+	cout << 1;
+
+	return 0;
+}
