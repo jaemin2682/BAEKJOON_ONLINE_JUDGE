d63b6c56e9bbbce993c8091b16d44b49dc9c3648
jaemin2682 <46991162+jaemin2682@users.noreply.github.com>
2019-11-14 23:17:09 +0900

    와이버스 부릉부릉

---
+++ 14645.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <memory.h>
+#include <math.h>
+#include <queue>
+
+using namespace std;
+
+int main() {
+	int n, k;
+	cin >> n >> k;
+	for (int i = 0; i < n; i++) {
+		int a, b;
+		cin >> a >> b;
+	}
+	cout << "ºñ¿ÍÀÌ";
+
+	return 0;
+}
