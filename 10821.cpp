9699f689dc0c3b83de8050e5ddc72298bf049c14
jaemin2682 <46991162+jaemin2682@users.noreply.github.com>
2019-11-18 16:19:36 +0900

    정수의 개수

---
+++ 10821.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string str;
+    cin >> str;
+    int cnt=0;
+    for(int i=0;i<str.size();i++) {
+        if(str[i] == ',') cnt++;
+    }
+    cout << cnt+1;
+
+    return 0;
+}
