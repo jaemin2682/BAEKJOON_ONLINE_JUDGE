8d7f67de9be49905bca2395e188dbc9ecfe0d5b4
jaemin2682 <46991162+jaemin2682@users.noreply.github.com>
2019-11-20 02:26:44 +0900

    운동장 한 바퀴

---
+++ 16486.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    double d1, d2;
+    cin >> d1 >> d2;
+    cout.precision(20);
+    cout << 2*d1 + 2*d2*3.141592;
+
+    return 0;
+}
