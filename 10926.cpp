81d8bec278d3d886789f6ffdcd4daee8d5fa305d
jaemin2682 <46991162+jaemin2682@users.noreply.github.com>
2019-11-26 20:48:30 +0900

    ??!

---
+++ 10926.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string str;
+
+int main() {
+    cin.sync_with_stdio(0);
+    cin.tie(0);
+    cin >> str;
+    cout << str + "\?\?!";
+
+    return 0;
+}
+// 간단한 출력 문제이다.
