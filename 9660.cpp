0d48d60b5b42dd4f98453471acf3e8dd4640ae3a
jaemin2682 <46991162+jaemin2682@users.noreply.github.com>
2019-12-08 16:05:51 +0900

    돌 게임 6

---
+++ 9660.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+long long n;
+
+int main() {
+    cin.sync_with_stdio(0);
+    cin.tie(0);
+    cin >> n;
+    if(n%7==0 || n%7==2) cout << "CY";
+    else cout << "SK";
+    return 0;
+}
