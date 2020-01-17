d0e6a81c2e4f070fd437c1288efce8ae62cfe046
JaeMin <46991162+jaemin2682@users.noreply.github.com>
2020-01-17 10:47:27 +0900

    직각삼각형

---
+++ 4153.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int a, b, c;
+
+int main() {
+	cin.sync_with_stdio(0);
+	cin.tie(0);
+	while (1) {
+		cin >> a >> b >> c;
+		if (a == 0 && b == 0 && c == 0) break;
+
+		if (a*a + b * b == c * c || a*a + c*c == b*b || b*b + c*c == a*a) cout << "right" << '\n';
+		else cout << "wrong" << '\n';
+	}
+	
+	return 0;
+}
